268e014a51881f6dffb6ca58ac7b4eee0c331e90
Denilson valero <denilsonvalero@gmail.com>
2025-10-27 16:05:03 -0300

    feat(comments): agregar rutas CRUD completas para comentarios

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const db = require('./config/DB.js');
+const commentsRouter = require('./routers/comments.js');
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
+app.use('/comments', commentsRouter);
 
 app.get('/', (req, res) => {
     res.send('Servidor funcionando correctamente');
