1e115fbf5f43c6118f33a18a6d5563e67da0318a
bryan marquez <bryan_amg1@icloud.com~>
2025-11-02 15:34:31 -0300

    crate router Post comment

--- src/app.js
+++ src/app.js
@@ -1,31 +1,33 @@
 
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const commentsRouter = require('./routers/comments.js');
-const userRouter = require('./routers/user.js');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import commentsRouter from './routers/comments.js';
+/* import userRouter from './routers/user.js'; */
 
-
-
+// Rutas absolutas y dotenv
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
-dotenv.config();
 
+// Configuración del servidor
 const app = express();
-
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Rutas
 app.use('/comments', commentsRouter);
-app.use('/users', userRouter);
+/* app.use('/users', userRouter); */
 
 app.get('/', (req, res) => {
-    res.send('Servidor funcionando correctamente');
+  res.send('Servidor funcionando correctamente');
 });
 
 app.listen(PORT, () => {
-    console.log(`servidor iniciado en el puerto:http://localhost:${PORT}`);
+  console.log(`✅ Servidor iniciado en: http://localhost:${PORT}`);
 });
