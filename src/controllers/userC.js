51f115dce9a0616a58f4781231594a12ccaeab2a
Denilson valero <denilsonvaleromail.com>
2025-11-03 19:11:43 -0300

    :recycle:/refactor_database_users

--- src/controllers/userC.js
+++ src/controllers/userC.js
@@ -5,9 +5,10 @@ const SECRET_KEY = process.env.SECRET_KEY || "2025jwtdev";
 
 export const getUser= async (req,res)=>{
     try{
-        const [users]= await db.query("SELECT * FROM user")
+        const [users]= await db.query("SELECT * FROM users")
+        
         res.status(200).json({msg:"usuarios" , data:users})
-        const userId=req.user.id;
+        const userId=req.users.id; 
         console.log("ID del usuario autenticado:", userId);
 
     }
@@ -19,12 +20,12 @@ export const getUser= async (req,res)=>{
 export const register= async (req,res)=>{
     const {name,email,password}= req.body;
     try{
-        const[existinguser]= await db.query("SELECT * FROM user WHERE email= ?", [email]);
+        const[existinguser]= await db.query("SELECT * FROM users WHERE email= ?", [email]);
         if(existinguser.length>0){
             return res.status(400).json({msg:"el usuario ya existe"})
         }else{
         const hashedPassword= await bcrypt.hash(password,10);
-        await db.query("INSERT INTO user (name,email,password) VALUES (?,?,?)", [name,email,hashedPassword]);
+        await db.query("INSERT INTO users (user_name,email,password) VALUES (?,?,?)", [name,email,hashedPassword]);
         res.status(201).json({msg:"usuario registrado con exito"});
         }
     }
@@ -36,16 +37,16 @@ export const register= async (req,res)=>{
 export const login= async (req,res)=>{
     const {email,password}=req.body;
     try{
-        const[existinguser]= await db.query("SELECT * FROM user WHERE email= ?", [email]);
+        const[existinguser]= await db.query("SELECT * FROM users WHERE email= ?", [email]);
         if(existinguser.length===0){
             return res.status(400).json({msg:"este usuario no existe"})
         }
-        const user=existinguser[0];
-        const ismacht= await bcrypt.compare(password,user.password)
+        const users=existinguser[0];
+        const ismacht= await bcrypt.compare(password,users.password)
         if(!ismacht){
             return res.status(400).json({msg:"contraseña incorrecta"})
         }
-    const token = jwt.sign({ user: { id: user.iduser, name: user.name, email: user.email } }
+    const token = jwt.sign({ users: { id: users.user_id, name: users.user_name, email: users.email } }
         ,SECRET_KEY,{ expiresIn: "1h" });
 
         res.status(200).json({msg: "login exitoso",token,});
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -10,7 +10,7 @@ const auth = (req, res, next) => {
     }
     try {
         const verificar = jwt.verify(token, SECRET_KEY);
-        req.user = verificar.user;
+        req.users = verificar.users;
         next();
     } catch (err) {
         res.status(401).json({msg:"el token no es valido"});
